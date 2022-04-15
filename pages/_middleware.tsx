76727862cfc1920f1051ee4f4ae32062982a3e0d
이지우 <leejiwoo2002@gmail.com>
2022-04-15 10:01:25 +0900

    fix : pages/middleware 불필요 로직 제거

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -1,22 +1,18 @@
-import { useRouter } from 'next/router';
-import { NextFetchEvent, NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
 
-const PUBLIC_FILE = /\.(.*)$/
+const PUBLIC_FILE = /\.(.*)$/;
 
 export function middleware(request: NextRequest) {
-  const supportLocales = ['ko','en','ja'];
-
   const shouldHandleLocale =
     !PUBLIC_FILE.test(request.nextUrl.pathname) &&
     !request.nextUrl.pathname.includes('/api/') &&
-    request.nextUrl.locale === 'default'
+    request.nextUrl.locale === 'default';
 
   if (shouldHandleLocale) {
-    const url = request.nextUrl.clone()
-    url.pathname = `/ko${request.nextUrl.pathname}`
-    return NextResponse.redirect(url)
+    const url = request.nextUrl.clone();
+    url.pathname = `/ko${request.nextUrl.pathname}`;
+    return NextResponse.redirect(url);
   }
 
-  return undefined
+  return undefined;
 }
