8cbfb2106ba2854f01b3fbf4494026cf052fe482
이지우 <leejiwoo2002@gmail.com>
2022-04-15 09:59:06 +0900

    fix : 미사용 css 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
-import { initReactI18next } from "react-i18next";
-import i18n from "i18next";
+import { initReactI18next } from 'react-i18next';
+import i18n from 'i18next';
 
 import en_lokalise from '../i18n/en.json';
 import ko_lokalise from '../i18n/ko.json';
@@ -11,25 +10,22 @@ import ja_lokalise from '../i18n/ja.json';
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
-  i18n
-    .use(initReactI18next)
-    .init({
-      resources: {
-        en: {
-          translation: en_lokalise
-        },
-        ko: {
-          translation: ko_lokalise
-        },
-        ja: {
-          translation: ja_lokalise
-        }
+  i18n.use(initReactI18next).init({
+    resources: {
+      en: {
+        translation: en_lokalise,
       },
-      lng: router.locale || 'ko'
-    });
+      ko: {
+        translation: ko_lokalise,
+      },
+      ja: {
+        translation: ja_lokalise,
+      },
+    },
+    lng: router.locale || 'ko',
+  });
 
-  
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
